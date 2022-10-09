7b03f4ad0b39758db0b26875dc851af19c4122a6
jnk0le <jnk0le@hotmail.com>
2022-10-09 22:08:16 +0200

    added 7 segment

---
+++ stm32_7segment/sseg.hpp
@@ -0,0 +1,346 @@
+/*!
+ * \file sseg.hpp
+ * \brief
+ *
+ *
+ * \author Jan Oleksiewicz <jnk0le@hotmail.com>
+ * \license SPDX-License-Identifier: MIT
+ * \date 9 oct 2022
+ */
+
+#ifndef SSEG_HPP
+#define SSEG_HPP
+
+#include <stdint.h>
+#include <stddef.h>
+#include <array>
+#include <type_traits>
+
+#include <cmsis_device.h>
+
+namespace jnk0le
+{
+namespace sseg
+{
+	template <uintptr_t gpio_addr, bool invert_polarity, int Apos, int Bpos, int Cpos, int Dpos, int Epos, int Fpos, int Gpos, int DPpos = 0xffff>
+	class PinConfig
+	{
+	public:
+		static inline constexpr GPIO_TypeDef* getSegHwReg() { //naming ??
+			return reinterpret_cast<GPIO_TypeDef*>(gpio_addr);
+		}
+
+		static inline constexpr uint32_t mapToBsrrPatternOff()
+		{
+			uint32_t tmp = (A | B | C | D | E | F | G) << 16;
+
+			if constexpr(invert_polarity)
+				tmp = (tmp >> 16) | (tmp << 16);
+
+			return tmp;
+		}
+
+		static inline constexpr uint32_t mapToBsrrPatternDigit(uint32_t i)
+		{
+			uint32_t tmp;
+
+			switch(i)
+			{
+			case 0:
+				tmp = A | B | C | D | E | F;
+				tmp |= G << 16;
+				break;
+			case 1:
+				tmp = B | C;
+				tmp |= (A | D | E | F | G) << 16;
+				break;
+			case 2:
+				tmp = A | B | D | E | G;
+				tmp |= (C | F) << 16;
+				break;
+			case 3:
+				tmp = A | B | C | D | G;
+				tmp |= (E | F) << 16;
+				break;
+			case 4:
+				tmp = B | C | F | G;
+				tmp |= (A | D | E) << 16;
+				break;
+			case 5:
+				tmp = A | C | D | F | G;
+				tmp |= (B | E) << 16;
+				break;
+			case 6:
+				tmp = A | C | D | E | F | G;
+				tmp |= B << 16;
+				break;
+			case 7:
+				tmp = A | B | C;
+				tmp |= (D | E | F | G) << 16;
+				break;
+			case 8:
+				tmp = A | B | C | D | E | F | G;
+				break;
+			case 9:
+				tmp = A | B | C | D | F | G;
+				tmp |= E << 16;
+				break;
+			case 10:
+				tmp = A | B | C | E | F | G;
+				tmp |= D << 16;
+				break;
+			case 11:
+				tmp =  C | D | E | F | G;
+				tmp |= (A | B) << 16;
+				break;
+			case 12:
+				tmp = A | D | E | F ;
+				tmp |=  (B | C | G) << 16;
+				break;
+			case 13:
+				tmp =  B | C | D | E | G;
+				tmp |= (A | F) << 16;
+				break;
+			case 14:
+				tmp = A | D | E | F | G;
+				tmp |= (B | C) << 16;
+				break;
+			case 15:
+				tmp = A | E | F | G;
+				tmp |=  (B | C | D) << 16;
+				break;
+			default: // turn off all ??
+				tmp = (A | B | C | D | E | F | G) << 16;
+			}
+
+			if constexpr(invert_polarity)
+				tmp = (tmp >> 16) | (tmp << 16);
+
+			return tmp;
+		}
+
+		//map to asci character/glyph????
+
+	private:
+		static constexpr int A = 1 << Apos;
+		static constexpr int B = 1 << Bpos;
+		static constexpr int C = 1 << Cpos;
+		static constexpr int D = 1 << Dpos;
+		static constexpr int E = 1 << Epos;
+		static constexpr int F = 1 << Fpos;
+		static constexpr int G = 1 << Gpos;
+		static constexpr int DP = 1 << DPpos; //???
+	};
+
+	template <bool invert_polarity, uintptr_t gpio_addr, uint32_t... args>
+	class CommonConfig
+	{
+	public:
+		static inline constexpr unsigned int getColumnAmount() {
+			return (sizeof...(args));
+		}
+
+		//all seems more efficient than another switch
+		static inline constexpr void turnOffAll()
+		{
+			if constexpr(invert_polarity)
+				reinterpret_cast<GPIO_TypeDef*>(gpio_addr)->BSRR = selectAllPinsMask();
+			else
+				reinterpret_cast<GPIO_TypeDef*>(gpio_addr)->BRR = selectAllPinsMask();
+		}
+
+		static inline constexpr void turnOn(unsigned int idx)
+		{
+			if constexpr(invert_polarity)
+				reinterpret_cast<GPIO_TypeDef*>(gpio_addr)->BRR = (1 << parseArgs<args...>(idx));
+			else
+				reinterpret_cast<GPIO_TypeDef*>(gpio_addr)->BSRR = (1 << parseArgs<args...>(idx));
+		}
+
+	private:
+
+		static inline constexpr uint32_t selectAllPinsMask()
+		{
+			uint32_t tmp = 0;
+
+			for(unsigned int i = 0; i<getColumnAmount(); i++)
+				tmp |= (1 << parseArgs<args...>(i));
+
+			return tmp;
+		}
+
+		//0 is first instead of 1
+		template<uint32_t Cpos, uint32_t... tail>
+		[[gnu::always_inline]] static inline constexpr uint32_t parseArgs(unsigned int finish_cond)
+		{
+			if(finish_cond > getColumnAmount()) {
+				__builtin_unreachable();
+				return 0;
+			} else if(finish_cond == 0) {
+				return Cpos;
+			} else {
+				if constexpr((sizeof...(tail) > 0)) // avoids extra boilerplate of empty tail overload
+					return parseArgs<tail...>(finish_cond-1);
+				else {
+					__builtin_unreachable();
+					return 0;
+				}
+			}
+		}
+
+		static_assert((getColumnAmount() > 0), "wrong number of parameters - provide pin position for each column");
+	};
+
+	//cache the column base/mask ????, then lookup by i
+	template <bool invert_polarity, /*uintptr_t gpio_addr, int Cpos,*/ uint32_t... args>
+	class CommonConfigScattered
+	{
+	public:
+		static inline constexpr unsigned int getColumnAmount() {
+			return (sizeof...(args))/2;
+		}
+
+		//all seems more efficient than another switch
+		static inline constexpr void turnOffAll() {
+			for(unsigned int i = 0; i < getColumnAmount(); i++) {
+				if constexpr(invert_polarity)
+					reinterpret_cast<GPIO_TypeDef*>(parseArgs<true, args...>(i))->BSRR = (1 << parseArgs<false, args...>(i));
+				else
+					reinterpret_cast<GPIO_TypeDef*>(parseArgs<true, args...>(i))->BRR = (1 << parseArgs<false, args...>(i));
+			}
+		}
+
+		static inline constexpr void turnOn(unsigned int idx)
+		{
+			if constexpr(invert_polarity)
+				reinterpret_cast<GPIO_TypeDef*>(parseArgs<true, args...>(idx))->BRR = (1 << parseArgs<false, args...>(idx));
+			else
+				reinterpret_cast<GPIO_TypeDef*>(parseArgs<true, args...>(idx))->BSRR = (1 << parseArgs<false, args...>(idx));
+		}
+
+	private:
+
+		//0 is first instead of 1
+		template<bool parse_addr, uint32_t gpio_addr, uint32_t Cpos, uint32_t... tail>
+		[[gnu::always_inline]] static inline constexpr uint32_t parseArgs(unsigned int finish_cond)
+		{
+			if(finish_cond > getColumnAmount()) {
+				__builtin_unreachable();
+				return 0;
+			} else if(finish_cond == 0) {
+				if constexpr(parse_addr)
+					return gpio_addr;
+				else
+					return Cpos;
+			} else {
+				if constexpr((sizeof...(tail) > 0)) // avoids extra boilerplate of empty tail overload
+					return parseArgs<parse_addr, tail...>(finish_cond-1);
+				else {
+					__builtin_unreachable();
+					return 0;
+				}
+			}
+		}
+
+		static_assert((getColumnAmount()%2 == 0), "wrong number of parameters - provide GPIO_BASE and pin position for each column");
+		static_assert((getColumnAmount() > 0), "wrong number of parameters - provide GPIO_BASE and pin position for each column");
+	};
+
+	template<class seg_config, class common_config, bool use_dot>
+	class Display
+	{
+	public:
+
+		Display()
+		{
+			for(unsigned int i = 0; i < common_config::getColumnAmount(); i++)
+				disp_cache[i] = seg_config::mapToBsrrPatternOff(); // clear all to avoid ruining gpio state
+		}
+
+		void defaultIrqHandler()
+		{
+			common_config::turnOffAll();
+
+			if(cnt == 0)
+				cnt = common_config::getColumnAmount();
+
+			cnt--;
+
+			//delay here in case of ghosting
+
+			seg_config::getSegHwReg()->BSRR = disp_cache[cnt];
+
+			common_config::turnOn(cnt);
+		}
+
+		template<bool mask_leading_zero = true, typename T>
+		void write(T num)
+		{
+			static_assert(std::is_integral_v<T>, "integers only");
+
+			for(unsigned int i = common_config::getColumnAmount(); i>0; i--) {
+				if(i < common_config::getColumnAmount() && num == 0 && mask_leading_zero)
+					disp_cache[i-1] = seg_config::mapToBsrrPatternOff();
+				else
+					disp_cache[i-1] = segment_lut_decimal[num % 10];
+
+				num /= 10;
+			}
+
+		}
+
+		template<bool mask_leading_zero = false, typename T>
+		void writeHex(T num)
+		{
+			static_assert(std::is_integral_v<T>, "integers only");
+
+			for(unsigned int i = common_config::getColumnAmount(); i>0; i--) {
+				if(i < common_config::getColumnAmount() && num == 0 && mask_leading_zero)
+					disp_cache[i-1] = seg_config::mapToBsrrPatternOff();
+				else
+					disp_cache[i-1] = segment_lut_hex[num & 15];
+
+				num >>= 4;
+			}
+
+		}
+
+		//write asci
+
+		//glyphs ????
+
+		//single update ????
+
+	private:
+
+		int cnt = 0;
+		volatile uint32_t disp_cache[common_config::getColumnAmount()];
+
+		//requires c++20, otherwise gets messy
+		static inline constexpr std::array<uint32_t, 10> segment_lut_decimal = []()
+		{
+			std::array<uint32_t, 10> arr;
+			for (uint32_t i = 0; i < arr.size(); i++) {
+				arr[i] = seg_config::mapToBsrrPatternDigit(i);
+			}
+
+			return arr;
+		}();
+
+		//hex and decimal is not usually mixed, keep as separate
+		static inline constexpr std::array<uint32_t, 16> segment_lut_hex = []()
+		{
+			std::array<uint32_t, 16> arr;
+			for (uint32_t i = 0; i < arr.size(); i++) {
+				arr[i] = seg_config::mapToBsrrPatternDigit(i);
+			}
+
+			return arr;
+		}();
+
+	};
+
+} // namespace
+} // namespace
+
+#endif /* SSEG_HPP */
---
+++ stm32_7segment/sseg_test.cpp
@@ -0,0 +1,122 @@
+/*!
+ * \file sseg_test.hpp
+ * \brief
+ *
+ *
+ * \author Jan Oleksiewicz <jnk0le@hotmail.com>
+ * \license SPDX-License-Identifier: MIT
+ * \date 9 oct 2022
+ */
+
+#include <stdio.h>
+#include <stm32f0xx.h>
+
+#include "sseg.hpp"
+
+//common cathode
+
+// common - drive by emitter follower pnp
+// CA1 - PF1
+// CA2 - PF0
+// CA3 - PA9
+// CA4 - PB1
+
+// segment - active high (drive directly)
+// seg A - PA4
+// seg B - PA2
+// seg C - PA6
+// seg D - PA5
+// seg E - PA1
+// seg F - PA3
+// seg G - PA7
+// seg DP - PA0
+
+
+using segment_config = jnk0le::sseg::PinConfig<GPIOA_BASE, false, 4, 2, 6, 5, 1, 3, 7, 0>;
+using common_config = jnk0le::sseg::CommonConfigScattered<true, GPIOF_BASE,1, GPIOF_BASE,0, GPIOA_BASE,9, GPIOB_BASE,1>;
+//using common_simple = jnk0le::sseg::CommonConfig<true, GPIOB_BASE, 2, 3, 4, 5>;
+
+jnk0le::sseg::Display<segment_config, common_config, true> displ;
+
+void sseg_init_all()
+{
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOFEN;
+
+	//initialize to disabled state ??
+
+	//common
+	GPIOB->MODER |= (0b01 << GPIO_MODER_MODER1_Pos);
+	GPIOF->MODER |= (0b01 << GPIO_MODER_MODER0_Pos) | (0b01 << GPIO_MODER_MODER1_Pos);
+	GPIOA->MODER |= (0b01 << GPIO_MODER_MODER9_Pos);
+
+	//segment
+	GPIOA->MODER |= (0b01 << GPIO_MODER_MODER4_Pos)
+					|(0b01 << GPIO_MODER_MODER2_Pos)
+					|(0b01 << GPIO_MODER_MODER6_Pos)
+					|(0b01 << GPIO_MODER_MODER5_Pos)
+					|(0b01 << GPIO_MODER_MODER1_Pos)
+					|(0b01 << GPIO_MODER_MODER3_Pos)
+					|(0b01 << GPIO_MODER_MODER7_Pos)
+					|(0b01 << GPIO_MODER_MODER0_Pos);
+
+	GPIOA->OSPEEDR |= (0b11 << GPIO_OSPEEDR_OSPEEDR4_Pos)
+					|(0b11 << GPIO_OSPEEDR_OSPEEDR2_Pos)
+					|(0b11 << GPIO_OSPEEDR_OSPEEDR6_Pos)
+					|(0b11 << GPIO_OSPEEDR_OSPEEDR5_Pos)
+					|(0b11 << GPIO_OSPEEDR_OSPEEDR1_Pos)
+					|(0b11 << GPIO_OSPEEDR_OSPEEDR3_Pos)
+					|(0b11 << GPIO_OSPEEDR_OSPEEDR7_Pos)
+					|(0b11 << GPIO_OSPEEDR_OSPEEDR0_Pos);
+
+	//48 mhz
+	RCC->APB2ENR |= RCC_APB2ENR_TIM16EN;
+
+	TIM16->DIER = TIM_DIER_UIE;
+
+	TIM16->ARR = 47999; // 1khz isr rate
+
+	TIM16->CR1 = TIM_CR1_CEN;
+
+	NVIC_EnableIRQ(TIM16_IRQn);
+}
+
+void sseg_test_loop()
+{
+	static int n = 0;
+	displ.write(n++);
+
+	for(volatile int i = 0; i<500000; i++);
+}
+
+extern "C" void TIM16_IRQHandler()
+{
+	TIM16->SR = 0; //bits are rc_w0, write ~UIF (1 to others)
+
+	displ.defaultIrqHandler();
+}
+
+void init_clocks() // 48mhz hsi
+{
+	FLASH->ACR |= FLASH_ACR_PRFTBE_Msk | (FLASH_ACR_LATENCY_Msk & 0b001); // 1ws
+
+	RCC->CFGR = RCC_CFGR_PLLMUL12; // 0 on reset, hsi/2, hsi selected
+
+	RCC->CR |= RCC_CR_PLLON;
+
+	while(!(RCC->CR & RCC_CR_PLLRDY));
+
+	RCC->CFGR |= RCC_CFGR_SW_PLL;
+	while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+}
+
+int main()
+{
+	init_clocks();
+
+	sseg_init_all();
+
+	while(1)
+	{
+		sseg_test_loop();
+	}
+}
